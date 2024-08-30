c0ef6024c223cb8e1a98277066e0a9b8d1f27906
Isaiahcancode <ITayl715@evit.edu>
2024-08-30 09:06:56 -0700

    exported from Skill Struck

---
+++ timer.py
@@ -0,0 +1,42 @@
+import pygame
+pygame.init()
+
+X = 400
+Y = 400
+#colors
+
+white = [255,255,255]
+black = [0, 0, 0]
+blue = (0,52,251)
+red = (255,15,15)
+backround = (0,52,92)
+#setup screen
+display = pygame.display.set_mode([X, Y])
+pygame.display.set_caption('Timer counting up')
+running = True
+
+clock = pygame.time.Clock()
+
+font = pygame.font.Font(None, 24)
+total_seconds = 0
+
+frame_count = 0
+while running:
+    display.fill(backround)
+
+    total_seconds = frame_count // 60
+    minutes = total_seconds // 60
+    seconds = total_seconds - (minutes * 60)
+    output_string = "timer: " + str(minutes) + ":" + str(seconds)
+
+    Text = font.render(output_string, True, white)
+    display.blit(Text, [150, 150])
+
+    
+
+    frame_count += 1
+
+    clock.tick(60)
+
+    
+    pygame.display.flip()
