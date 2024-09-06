1fad1e67a47acb15a120af1a2241869c578b1dcb
Isaiahcancode <ITayl715@evit.edu>
2024-09-06 09:57:28 -0700

    exported from Skill Struck

---
+++ mouse click.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+
+X = 500
+Y = 500
+BLUE = (90, 90, 250)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+cir_x = 150
+cir_y = 150
+
+DISPLAY = pygame.display.set_mode([X, Y])
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            cir_x = cir_x + 20
+
+    DISPLAY.fill(BLUE)
+    pygame.draw.circle(DISPLAY, WHITE, (cir_x, cir_y), 20)
+
+    pygame.display.flip()
+
+pygame.quit()
